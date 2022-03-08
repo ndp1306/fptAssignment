6ede2581bdb27890eea47c6a0cff8c545d0072b8
ndp1306 <73115190+ndp1306@users.noreply.github.com>
2022-03-08 11:10:47 +0700

    Delete assignment1 directory

--- assignment1/ArithmeticExercise3.java
+++
@@ -1,7 +0,0 @@
-package fa.training.assignment1;
-
-public class ArithmeticExercise3 {
-    public static void main(String[] args) {
-        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
-    }
-}
